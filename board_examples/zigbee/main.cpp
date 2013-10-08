05e22bd9f0f4f0a8a26a3e6548fb9a4d01606113
jp <jp@r-3-t.org>
2013-10-08 22:21:17 +0200

    add some doc for zigbee example

--- board_examples/zigbee/main.cpp
+++ board_examples/zigbee/main.cpp
@@ -1,6 +1,3 @@
-
-#include <stdlib.h>
-
 /*
 
     ZIGBEE AT command :
@@ -82,10 +79,48 @@
 
 */
 
+#include <hal/clock.hpp>
+#include <hal/uart.hpp>
+
+#include <stdio.h>
+
+#define UNUSED(expr) do { (void)(expr); } while (0)
+
+#define CURRENT_UART  1
+
+//------------------------------------------------------------------------------
+
+void callback (const std::string& buffer, uart::Uart& uart)
+{
+  uart.send(buffer.c_str());
+  uart.send("\r\n");
+}
+
+//------------------------------------------------------------------------------
+
+extern "C"
+{
+void write_stdout (const unsigned char* ptr, unsigned int len)
+{
+  uart::get_instance(CURRENT_UART).send(ptr, len);
+}
+}
+//------------------------------------------------------------------------------
+
 int main(void)
 {
+  uart::init_instance<uart::PolicyNotifyOnChar<'\r'>>(CURRENT_UART, callback);
 
-	while (1);
-
-	return 1;
+  unsigned char Idx = 0;
+  while(1)
+  {
+    printf ("Index : %d\r\n", Idx);
+    clock::usleep(1500);
+    Idx++;
+  }
 }
+
+//------------------------------------------------------------------------------
+
+
+
