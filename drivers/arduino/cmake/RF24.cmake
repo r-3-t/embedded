ed71ea9a653a25af5a5d5e361de9cf4fcfcd0146
keax <keax@keax.com>
2013-10-21 21:24:36 +0200

    Fix RF24 submodule :  - Add binary.h from arduino  - Remove on the fly typedef char const char

--- drivers/arduino/cmake/RF24.cmake
+++ drivers/arduino/cmake/RF24.cmake
@@ -3,3 +3,11 @@ list(APPEND PROJECT_SOURCES ${CMAKE_CURRENT_LIST_DIR}/../RF24/RF24.cpp)
 
 include_directories(${CMAKE_CURRENT_LIST_DIR}/../RF24/)
 
+EXECUTE_PROCESS(COMMAND sed -i "s,typedef char const char;,,e" ${CMAKE_CURRENT_LIST_DIR}/../RF24/RF24_config.h OUTPUT_VARIABLE DATETIMESTRING)
+
+file (WRITE ${CMAKE_CURRENT_LIST_DIR}/../RF24/WProgram.h
+"#include <frontend_arduino.hpp>
+using namespace ::arduino::gpio;
+using namespace ::arduino::clock;
+using namespace ::arduino::spi;
+")
