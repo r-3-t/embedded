722da96772f9414e5c178c234fc74b0a93a54bd0
keax <keax@keax.com>
2013-10-25 14:33:14 +0200

    TI LM4F120XL clock improvements : - Set board system clock to 80 Mhz (16 Mhz previously). - Improve systicks count algorithm during delays.

--- backends/ti/generic_code/hal/hal_lm4f120_clock.cpp
+++ backends/ti/generic_code/hal/hal_lm4f120_clock.cpp
@@ -2,14 +2,29 @@
 #include <types.hpp>
 #include <lm4f120h5qr.h>
 #include <sysctl.h>
+#include <systick.h>
 
-uint32_t gTickFactor;
+uint32_t guTickFactor;
+uint32_t gmTickFactor;
 
 struct AutoInitSysTick
 {
 	AutoInitSysTick()
 	{
-	    gTickFactor = SysCtlClockGet() / 1000000;
+	    //
+	    // Set the clocking to run directly from the PLL at 80 MHz
+	    //
+	    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
+	                               SYSCTL_OSC_MAIN);
+
+	    //we set a period with to max representation of uint16_t
+	    SysTickPeriodSet(0x0000FFFF + 1);
+
+	    //enable systick counter
+	    SysTickEnable();
+
+	    guTickFactor = SysCtlClockGet() / 1000000;
+	    gmTickFactor = SysCtlClockGet() / 1000;
 
 	}
 };
@@ -24,30 +39,47 @@ namespace clock
 		return NVIC_ST_CURRENT_R;
 	}
 
+	void decrease_ticks(uint32_t nbTicks)
+	{
+		uint16_t PreviousTickCounter = getTickCount();
+		uint16_t CurrentTickCounter;
+
+		//ticks count in one iteration
+		uint16_t elapseTicks = 0;
+
+		while( nbTicks > elapseTicks)
+		{
+			//decrease total ticks counter
+			nbTicks -= elapseTicks;
+
+			//calculate nb ticks between now and previous iteration
+			CurrentTickCounter = getTickCount();
+			elapseTicks = PreviousTickCounter - CurrentTickCounter;
+			PreviousTickCounter = CurrentTickCounter;
+
+		}
+	}
+
 	void usleep(unsigned int nTime)
 	{
-		//WARN : max counter value is 0xFFFFFF !!!
-		//TODO: maybe we should decrement (nTime * gTickFactor) ?
-
-		uint32_t start_TickCount = getTickCount();
-
-		while( (start_TickCount - getTickCount()) <  (nTime * gTickFactor));
+		//ticks count for the requested time
+		decrease_ticks(nTime * guTickFactor);
 	}
 
 	void msleep(unsigned int nTime)
 	{
-		unsigned int		i;
-
-		//TODO: use another timer and prescale to ms.
-		for (i = 0; i < nTime; i++)
-		{
-			usleep(1000);
-		}
+		//ticks count for the requested time
+		decrease_ticks(nTime * gmTickFactor);
 	}
 
 	unsigned int getTickPerMs()
 	{
-		return gTickFactor * 1000;
+		return gmTickFactor;
+	}
+
+	unsigned int getTickPerUs()
+	{
+		return guTickFactor;
 	}
 
 }
