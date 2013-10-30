fe19287960969dea8f4ac17e23453ea7d6082981
jp <jp@r-3-t.org>
2013-10-30 15:16:04 +0100

    add a helper to include drivers files

--- board_examples/cmake/example.cmake
+++ board_examples/cmake/example.cmake
@@ -2,6 +2,17 @@
 set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../backends/cmake/)
 include(embedded)
 
+if (PROJECT_DRIVERS)
+	foreach (driver ${PROJECT_DRIVERS})
+		if (EXISTS ${CMAKE_CURRENT_LIST_DIR}/../../drivers/${driver})
+			include_directories(${CMAKE_CURRENT_LIST_DIR}/../../drivers/${driver}/)
+			file (GLOB cpp_driver_files ${CMAKE_CURRENT_LIST_DIR}/../../drivers/${driver}/*.cpp)
+			file (GLOB hpp_driver_files ${CMAKE_CURRENT_LIST_DIR}/../../drivers/${driver}/*.hpp)
+			list (APPEND PROJECT_SOURCES ${cpp_driver_files} ${hpp_driver_files})
+		endif()
+	endforeach()
+endif()
+
 ADD_EXECUTABLE(${CMAKE_PROJECT_NAME}.elf		${PROJECT_SOURCES})
 
 ADD_CUSTOM_TARGET(${CMAKE_PROJECT_NAME}.bin ALL DEPENDS ${CMAKE_PROJECT_NAME}.elf COMMAND ${OBJCOPY} -O binary ${CMAKE_PROJECT_NAME}.elf ${CMAKE_PROJECT_NAME}.bin)
