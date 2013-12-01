9ff79d9f88dcb9a9b14ba07f75b005692f45b12a
keax <keax@keax.com>
2013-12-01 21:15:53 +0100

    Modify examples to use only global objects.

--- board_examples/pwm/main.cpp
+++ board_examples/pwm/main.cpp
@@ -1,34 +1,41 @@
+/******************************************************************************\
+		INCLUDES
+\******************************************************************************/
+#include <hal/Clock.hpp>
+#include <hal/Pwm.hpp>
 
-#include <hal/pwm.hpp>
-#include <hal/clock.hpp>
-
+/******************************************************************************\
+		DEFINES
+\******************************************************************************/
 #define PWM_PORT							1
 #define PWM_PIN								1
 
-//------------------------------------------------------------------------------
+/******************************************************************************\
+		GLOBALS
+\******************************************************************************/
+//initialize pwm with frequency of 100hz, duty can change from 0 to 100
+::pwm::ConcretePwm pwm0(PWM_PORT, PWM_PIN, 100/*Hz*/, 100 /* From 0 to 100 */);
 
+/******************************************************************************\
+		FUNCTIONS
+\******************************************************************************/
 int main(void)
 {
-	//initialize pwm with frequency of 100hz, duty can change from 0 to 100
-	::pwm::init_instance(PWM_PORT, PWM_PIN, 100/*Hz*/, 100 /* From 0 to 100 */);
-
-	::pwm::Pwm& pwm = *::pwm::get_instance(PWM_PORT, PWM_PIN);
-
 	while (1)
 	{
-		pwm.setDuty(0); // 0%
+		pwm0.setDuty(0); // 0%
 		clock::msleep(1000);
 
-		pwm.setDuty(25); // 25%
+		pwm0.setDuty(25); // 25%
 		clock::msleep(1000);
 
-		pwm.setDuty(50); // 50%
+		pwm0.setDuty(50); // 50%
 		clock::msleep(1000);
 
-		pwm.setDuty(75); // 75%
+		pwm0.setDuty(75); // 75%
 		clock::msleep(1000);
 
-		pwm.setDuty(100); // 100%
+		pwm0.setDuty(100); // 100%
 		clock::msleep(1000);
 	}
 }
