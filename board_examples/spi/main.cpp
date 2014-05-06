42aefc9c258e529f1b1a3adb0f2c5132bb388975
keax <keax@keax.com>
2014-05-06 19:03:01 +0200

    Fix SPI example with new lib interface.

--- board_examples/spi/main.cpp
+++ board_examples/spi/main.cpp
@@ -1,76 +1,58 @@
-
-#include <hal/clock.hpp>
-#include <hal/uart.hpp>
-#include <hal/spi.hpp>
-
+/******************************************************************************\
+		INCLUDES
+\******************************************************************************/
+#include <UartInterface.hpp>
+#include <SpiInterface.hpp>
 #include <stdio.h>
 
-#define UNUSED(expr) do { (void)(expr); } while (0)
+/******************************************************************************\
+		PROTOYPES
+\******************************************************************************/
+void uart_callback (uint8_t c);
+void spi_slave_callback(uint8_t c);
 
-#define CURRENT_UART  1
+/******************************************************************************\
+		GLOBALES
+\******************************************************************************/
+::spi::ConcreteSpi			Spi1(1, 0,::spi::Configuration::_default_master());
+::spi::ConcreteSpi			Spi2(2, spi_slave_callback,::spi::Configuration::_default_slave());
 
-//------------------------------------------------------------------------------
+::uart::ConcreteUart 		UartCommand(1, &uart_callback);
 
-void callback (const types::buffer& buffer, uart::Uart& uart)
+/******************************************************************************\
+		DEFINES
+\******************************************************************************/
+
+/******************************************************************************\
+		FUNCTIONS
+\******************************************************************************/
+
+void uart_callback (uint8_t c)
 {
-  uart.send(buffer);
-  uart.send("\r\n");
+  printf("Send to SPI : %02X\r\n", c);
+  
 }
 
-void spi_callback(spi::Spi& spi)
+void spi_slave_callback(uint8_t c)
 {
-  printf ("recv ... \r\n");
-  uint8_t c = spi.recv();
-  printf ("spi2 recv : %02X\r\n", c);
+  printf ("spi slave recv : %02X\r\n", c);
 }
-
-//------------------------------------------------------------------------------
 
 extern "C"
 {
 void write_stdout (const unsigned char* ptr, unsigned int len)
 {
-  uart::get_instance(CURRENT_UART).send(ptr, len);
+  UartCommand.send(ptr, len);
 }
 }
 
 int main ()
 {
 
-  uart::init_instance<uart::PolicyNotifyOnChar<'\r'>>(CURRENT_UART, callback);
-
-  using namespace spi;
-  spi::init_instance(1,     { Configuration::_2Lines_FullDuplex, 
-                              Configuration::Master,
-                              Configuration::Low,
-                              Configuration::_1_Edge,
-                              Configuration::Soft,
-                              Configuration::_2,
-                              Configuration::MSB,
-                              7
-                            });
-
-  spi::init_instance(2,     { Configuration::_2Lines_FullDuplex, 
-                              Configuration::Slave,
-                              Configuration::Low,
-                              Configuration::_1_Edge,
-                              Configuration::Soft,
-                              Configuration::_2,
-                              Configuration::MSB,
-                              7
-                            });
-
-
-
-
-  spi::Spi& spi1 = get_instance(1);
-  spi::Spi& spi2 = get_instance(2);
-  spi2.register_callback(spi_callback);
-
   for (unsigned int i = 0; i < 0xFF; i++)
   {
     printf ("send ... %02X\r\n", i);
-    spi1.send (i);
+    Spi1.send (i);
   }
 
   while (1);
