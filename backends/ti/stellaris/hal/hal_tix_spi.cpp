2bf9fd7701b4779d6d1b5a658909e057fbc453fe
keax <keax@keax.com>
2014-05-14 22:48:39 +0200

    Fix SPI TI hal with new interface.

---
+++ backends/ti/stellaris/hal/hal_tix_spi.cpp
@@ -0,0 +1,242 @@
+#include <usart_conf.hpp>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <hw_types.h>
+#include <hw_memmap.h>
+#include <SpiInterface.hpp>
+#include <ssi.h>
+#include <interrupt.h>
+#include <hw_ints.h>
+
+
+#define TIXX_SPI_MAX_COUNT								3
+
+namespace tix
+{
+
+	namespace spi
+	{
+
+		static ::tix::spi::Spi*				gpSpis[TIXX_SPI_MAX_COUNT];
+
+		#define SPI_IRQ_CALLBACK(SpiX)	void SPI##SpiX##_IRQHandler()																\
+											{																								\
+												unsigned long status;																		\
+												status = SSIIntStatus(gpSpis[SpiX]->SSI_Base, false);								\
+												if ((status & SSI_RXFF) == SSI_RXFF)													\
+												{																							\
+													gpSpis[SpiX]->_callback(gpSpis[SpiX]->recv());							\
+												}																							\
+												if ((status & SSI_RXTO) == SSI_RXTO)													\
+												{																							\
+													gpSpis[SpiX]->_callback(gpSpis[SpiX]->recv());							\
+												}																							\
+												status = SSIIntStatus(gpSpis[SpiX]->SSI_Base, true);								\
+												SSIIntClear(gpSpis[SpiX]->SSI_Base, status);										\
+											}
+
+		extern "C"
+		{
+
+				SPI_IRQ_CALLBACK(0)
+				SPI_IRQ_CALLBACK(1)
+				SPI_IRQ_CALLBACK(2)
+
+		}
+
+		Spi::Spi(unsigned int id, ::spi::SpiInterface::spi_callback_T callback, ::spi::Configuration config)
+		{
+			_id = id;
+			_callback = callback;
+			_config = config;
+
+			gpSpis[id - 1] = this;
+			
+			this->Configure();
+		}
+
+		void Spi::Configure()
+		{
+
+			//GPIO configuration for SSI use
+			unsigned long ClkPinConfig;
+			unsigned long FssPinConfig;
+			unsigned long RxPinConfig;
+			unsigned long TxPinConfig;
+
+			//clocks associated to UARTs
+			unsigned long SSIPeripheral;
+			unsigned long GPIOPeripheral;
+
+			// the gpio used
+			unsigned long 		GPIO_Port;
+			unsigned char		GPIO_Pin_FSS;			// the selected pin
+			unsigned char		GPIO_Pin_SCK;			// the selected pin
+			unsigned char		GPIO_Pin_MISO;			// the selected pin
+			unsigned char		GPIO_Pin_MOSI;			// the selected pin
+
+			switch (_id)
+			{
+				case 1:
+					//SSI base
+					SSI_Base					= SSI0_BASE;
+
+					//Peripheral clocks
+					SSIPeripheral				= SYSCTL_PERIPH_SSI0;
+					GPIOPeripheral				= SYSCTL_PERIPH_GPIOA;
+
+					//Configure GPIO for SSI
+					ClkPinConfig				= GPIO_PA2_SSI0CLK;
+					FssPinConfig				= GPIO_PA3_SSI0FSS;
+					RxPinConfig					= GPIO_PA4_SSI0RX;
+					TxPinConfig					= GPIO_PA5_SSI0TX;
+
+					//GPIO for SSI pins
+					GPIO_Port					= GPIO_PORTA_BASE;
+					GPIO_Pin_FSS				= GPIO_PIN_3;
+					GPIO_Pin_SCK				= GPIO_PIN_2;
+					GPIO_Pin_MISO				= GPIO_PIN_4;
+					GPIO_Pin_MOSI				= GPIO_PIN_5;
+
+					SSIIntRegister(SSI_Base, SPI0_IRQHandler);
+
+					break;
+
+				case 2:
+					//SSI base
+					SSI_Base					= SSI1_BASE;
+
+					//Peripheral clocks
+					SSIPeripheral				= SYSCTL_PERIPH_SSI1;
+					GPIOPeripheral				= SYSCTL_PERIPH_GPIOD;
+
+					//Configure GPIO for SSI
+					ClkPinConfig				= GPIO_PD0_SSI1CLK;
+					FssPinConfig				= GPIO_PD1_SSI1FSS;
+					RxPinConfig					= GPIO_PD2_SSI1RX;
+					TxPinConfig					= GPIO_PD3_SSI1TX;
+
+					//GPIO for SSI pins
+					GPIO_Port					= GPIO_PORTD_BASE;
+					GPIO_Pin_FSS				= GPIO_PIN_1;
+					GPIO_Pin_SCK				= GPIO_PIN_0;
+					GPIO_Pin_MISO				= GPIO_PIN_2;
+					GPIO_Pin_MOSI				= GPIO_PIN_3;
+
+					SSIIntRegister(SSI_Base, SPI1_IRQHandler);
+
+				case 3:
+					//SSI base
+					SSI_Base					= SSI2_BASE;
+
+					//Peripheral clocks
+					SSIPeripheral				= SYSCTL_PERIPH_SSI2;
+					GPIOPeripheral				= SYSCTL_PERIPH_GPIOB;
+
+					//Configure GPIO for SSI
+					ClkPinConfig				= GPIO_PB4_SSI2CLK;
+					FssPinConfig				= GPIO_PB5_SSI2FSS;
+					RxPinConfig					= GPIO_PB6_SSI2RX;
+					TxPinConfig					= GPIO_PB7_SSI2TX;
+
+					//GPIO for SSI pins
+					GPIO_Port					= GPIO_PORTB_BASE;
+					GPIO_Pin_FSS				= GPIO_PIN_5;
+					GPIO_Pin_SCK				= GPIO_PIN_4;
+					GPIO_Pin_MISO				= GPIO_PIN_6;
+					GPIO_Pin_MOSI				= GPIO_PIN_7;
+
+					SSIIntRegister(SSI_Base, SPI2_IRQHandler);
+
+					break;
+
+
+				default:
+					fprintf(stderr, "Unimplemented spi %i !\n", _id);
+					abort();
+					break;
+			}
+
+
+			//
+			// Enable the peripherals
+			//
+			SysCtlPeripheralEnable(GPIOPeripheral);
+			SysCtlPeripheralEnable(SSIPeripheral);
+
+
+			//
+			// Set GPIO for SSI pins.
+			//
+			GPIOPinConfigure(ClkPinConfig);
+			GPIOPinConfigure(FssPinConfig);
+			GPIOPinConfigure(RxPinConfig);
+			GPIOPinConfigure(TxPinConfig);
+
+			GPIOPinTypeSSI(GPIO_Port, GPIO_Pin_FSS | GPIO_Pin_SCK | GPIO_Pin_MISO | GPIO_Pin_MOSI);
+
+			//master
+			SSIConfigSetExpClk(SSI_Base, SysCtlClockGet(), (conf_2_ClockPolarity(_config.clockPolarity) << 1) | conf_2_ClockPhase(_config.clockPhase),
+					conf_2_mode(_config.mode), _config.DataRate, 8);
+
+			//
+			// Enable interrupt on RX event
+			//
+			SSIIntEnable(SSI_Base, SSI_RXFF | SSI_RXTO);
+
+
+			//enable SSI
+			SSIEnable(SSI_Base);
+
+			_configured = true;
+		}
+
+		uint8_t Spi::send(uint8_t data)
+		{
+			unsigned long   result;
+			unsigned long	data_8;
+
+			//SSIDataPut is blocking
+			data_8 = data & 0xFF;
+			SSIDataPut(SSI_Base, data_8);
+
+			SSIDataGet(SSI_Base, &result);
+
+			//wait end of transmission
+			while (SSIBusy(SSI_Base) == true);
+
+			return result & 0xFF;
+		}
+
+		uint8_t Spi::recv()
+		{
+			unsigned long   result;
+
+			SSIDataGet(SSI_Base, &result);
+
+			return result & 0xFF;
+		}
+
+		void Spi::send(const types::buffer& buf)
+		{
+			for (auto c : buf)
+			{
+				send(c & 0xFF);
+			}
+
+		}
+
+		void Spi::send(const unsigned char* buf, unsigned int length)
+		{
+			for (unsigned int i = 0; i < length; i++)
+			{
+				send(buf[i] & 0xFF);
+			}
+		}
+
+
+	} /* namespace spi  */
+
+} /* namespace tix  */
+
--- backends/ti/stellaris/hal/hal_tix_spi.hpp
+++ backends/ti/stellaris/hal/hal_tix_spi.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <spi.hpp>
-#include <hal/spi_conf.hpp>
+#include <SpiInterface.hpp>
+#include <spi_conf.hpp>
 #include <hw_memmap.h>
 #include <gpio.h>
 #include <ssi.h>
@@ -12,189 +12,31 @@ namespace  spi {
 
 	using namespace tix::spi;
 
-	class tix_Spi : public ::spi::Spi
-	{
-	public:
-		tix_Spi(unsigned int id, ::spi::Configuration config) :
-							::spi::Spi(id)
+		class Spi : public ::spi::SpiInterface
 		{
-			//GPIO configuration for SSI use
-			unsigned long ClkPinConfig;
-			unsigned long FssPinConfig;
-			unsigned long RxPinConfig;
-			unsigned long TxPinConfig;
+		public:
 
-			//clocks associated to UARTs
-			unsigned long SSIPeripheral;
-			unsigned long GPIOPeripheral;
+			Spi();
+			Spi(unsigned int id, spi_callback_T spi_callback, ::spi::Configuration config);
 
-			// the gpio used
-			unsigned long 		GPIO_Port;
-			unsigned char		GPIO_Pin_FSS;			// the selected pin
-			unsigned char		GPIO_Pin_SCK;			// the selected pin
-			unsigned char		GPIO_Pin_MISO;			// the selected pin
-			unsigned char		GPIO_Pin_MOSI;			// the selected pin
+			virtual uint8_t send(uint8_t data);;
+			virtual uint8_t recv();;
+			virtual void send(const types::buffer& buf);
+			virtual void send(const unsigned char* buf, unsigned int length);
 
-			switch (id)
-			{
-				case 1:
-					//SSI base
-					_SSI_Base					= SSI0_BASE;
+			unsigned long	  		SSI_Base;
 
-					//Peripheral clocks
-					SSIPeripheral				= SYSCTL_PERIPH_SSI0;
-					GPIOPeripheral				= SYSCTL_PERIPH_GPIOA;
+			void		 			Configure();
 
-					//Configure GPIO for SSI
-					ClkPinConfig				= GPIO_PA2_SSI0CLK;
-					FssPinConfig				= GPIO_PA3_SSI0FSS;
-					RxPinConfig					= GPIO_PA4_SSI0RX;
-					TxPinConfig					= GPIO_PA5_SSI0TX;
-
-					//GPIO for SSI pins
-					GPIO_Port					= GPIO_PORTA_BASE;
-					GPIO_Pin_FSS				= GPIO_PIN_3;
-					GPIO_Pin_SCK				= GPIO_PIN_2;
-					GPIO_Pin_MISO				= GPIO_PIN_4;
-					GPIO_Pin_MOSI				= GPIO_PIN_5;
-
-					break;
-
-				case 2:
-					//SSI base
-					_SSI_Base					= SSI1_BASE;
-
-					//Peripheral clocks
-					SSIPeripheral				= SYSCTL_PERIPH_SSI1;
-					GPIOPeripheral				= SYSCTL_PERIPH_GPIOD;
-
-					//Configure GPIO for SSI
-					ClkPinConfig				= GPIO_PD0_SSI1CLK;
-					FssPinConfig				= GPIO_PD1_SSI1FSS;
-					RxPinConfig					= GPIO_PD2_SSI1RX;
-					TxPinConfig					= GPIO_PD3_SSI1TX;
-
-					//GPIO for SSI pins
-					GPIO_Port					= GPIO_PORTD_BASE;
-					GPIO_Pin_FSS				= GPIO_PIN_1;
-					GPIO_Pin_SCK				= GPIO_PIN_0;
-					GPIO_Pin_MISO				= GPIO_PIN_2;
-					GPIO_Pin_MOSI				= GPIO_PIN_3;
-
-					break;
-
-
-				default:
-					//SSI base
-					_SSI_Base					= -1;
-
-					//Peripheral clocks
-					SSIPeripheral				= -1;
-					GPIOPeripheral				= -1;
-
-					//Configure GPIO for SSI
-					ClkPinConfig				= -1;
-					FssPinConfig				= -1;
-					RxPinConfig					= -1;
-					TxPinConfig					= -1;
-
-					//GPIO for SSI pins
-					GPIO_Port					= -1;
-					GPIO_Pin_FSS				= -1;
-					GPIO_Pin_SCK				= -1;
-					GPIO_Pin_MISO				= -1;
-					GPIO_Pin_MOSI				= -1;
-
-					return;
-					break;
-			}
-
-
-			//
-			// Enable the peripherals
-			//
-			SysCtlPeripheralEnable(GPIOPeripheral);
-			SysCtlPeripheralEnable(SSIPeripheral);
-
-
-			//
-			// Set GPIO for SSI pins.
-			//
-			GPIOPinConfigure(ClkPinConfig);
-			GPIOPinConfigure(FssPinConfig);
-			GPIOPinConfigure(RxPinConfig);
-			GPIOPinConfigure(TxPinConfig);
-
-			GPIOPinTypeSSI(GPIO_Port, GPIO_Pin_FSS | GPIO_Pin_SCK | GPIO_Pin_MISO | GPIO_Pin_MOSI);
-
-			//master
-			SSIConfigSetExpClk(_SSI_Base, SysCtlClockGet(), (conf_2_ClockPolarity(config.clockPolarity) << 1) | conf_2_ClockPhase(config.clockPhase),
-					conf_2_mode(config.mode), config.DataRate, 8);
-
-
-
-
-			//enable SSI
-			SSIEnable(_SSI_Base);
-
-		}
-
-		virtual uint8_t send(uint8_t data)
-		{
-			unsigned long   result;
-			unsigned long	data_8;
-
-			//SSIDataPut is blocking
-			data_8 = data & 0xFF;
-			SSIDataPut(_SSI_Base, data_8);
-
-			SSIDataGet(_SSI_Base, &result);
-
-			//wait end of transmission
-			while (SSIBusy(_SSI_Base) == true);
-
-			return result & 0xFF;
-		}
-
-		virtual uint8_t recv()
-		{
-			unsigned long   result;
-			
-			SSIDataGet(_SSI_Base, &result);
-
-			return result & 0xFF;
-		}
-
-		virtual void register_callback(void (*callback)(::spi::Spi&))
-		{
-			_callback = callback;
-
-
-		}
 		private:
-			unsigned long	  		_SSI_Base;
-	};
 
 
-	static ::spi::Spi* gpSpi_1 = nullptr;
-	static ::spi::Spi* gpSpi_2 = nullptr;
+			unsigned int			_id;
+			::spi::Configuration	_config;
+			bool					_configured;
 
-/*
- //TODO: implement callbacks and interrupts
-	#define SPI_IRQ_CALLBACK(SpiX)	void SPI##SpiX##_IRQ_Handler()									\
-									{																\
-										if (SPI_I2S_GetITStatus(SPI##SpiX, SPI_I2S_IT_RXNE) == SET)	\
-										{															\
-											::spi::Spi* spi = gpSpi_##SpiX;							\
-									    	spi->_callback(*spi);									\
-										}															\
-									}
+		};
 
-
-	SPI_IRQ_CALLBACK(1)
-	SPI_IRQ_CALLBACK(2)
-*/
-	static ::spi::Spi* gpSpi_NULL = nullptr;
 
 	} /* namespace spi  */
 
@@ -203,27 +45,6 @@ namespace  spi {
 namespace spi
 {
 
-	unsigned int	num_instance()
-	{
-		return 2;
-	}
+	typedef ::tix::spi::Spi ConcreteSpi;
 
-	void init_instance(unsigned int id, Configuration config)
-	{
-		switch (id)
-		{
-			case 1: ::tix::spi::gpSpi_1 = new ::tix::spi::tix_Spi(1, config); break;
-			case 2: ::tix::spi::gpSpi_2 = new ::tix::spi::tix_Spi(2, config); break;
-		}
-	}
-
-	Spi& get_instance(unsigned int id)
-	{
-		switch (id)
-		{
-			case 1: return *::tix::spi::gpSpi_1;
-			case 2: return *::tix::spi::gpSpi_2;
-		}
-		return *::tix::spi::gpSpi_NULL;
-	}
 }
