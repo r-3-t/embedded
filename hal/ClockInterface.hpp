b09abae2a45aaacb3b2daeea4111cd541c1c4a7c
keax <keax@keax.com>
2014-08-03 20:55:07 +0200

    (HAL) Add getTickPerUs function in clock interface.

--- hal/ClockInterface.hpp
+++ hal/ClockInterface.hpp
@@ -4,6 +4,7 @@ namespace clock
 {
 	unsigned int getTickCount();
 	unsigned int getTickPerMs();
+	unsigned int getTickPerUs();
 
 	void msleep(unsigned int nTime);
 	void usleep(unsigned int nTime);
